b2d53552478c842814c0dfad307222a54e20ff32
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-01-31 10:44:44 +0300

    Enqueue error to redis when status code > 0

--- nu-notify.scm
+++ nu-notify.scm
@@ -1,24 +1,15 @@
-(use-modules (redis))
+(use-modules (ice-9 format)
+             (redis))
+
 (define (enqueue-error error-message conn)
   (begin
     (redis-send conn (lpush `(queue:error ,error-message)))
     (redis-close conn)))
 
 (define (run-unix-cmd cmd)
-  ;;; Print to stdout on error
-  ;;; TODO: save to local redis queue instead
+  ;;; Run command command and on error,
+  ;;; enqueue the PID to a redis queue
   (let ((pid (system cmd)))
     (if (> (status:exit-val pid) 0)
-        (begin
-          (display "Pid value:") (display pid)
-          (newline)
-          (display "Status exit value:") (display (status:exit-val pid)) (newline)
-          (display "Status term sig:") (display (status:term-sig pid)) (newline)
-          (display "Status stop sig:") (display (status:stop-sig pid)) (newline)
-          ))
-    )
-  )
-
-(define conn (redis-connect))
-(redis-send conn (lpush '(queue:error "error-log")))
-(redis-close conn)
+        (enqueue-error (format #f "[Error] PID: ~d Command: ~a" pid cmd)
+                 (redis-connect)))))
