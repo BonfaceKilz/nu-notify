21a934448fcb69a43cbcfea08e1f8c4a5b74a028
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-01-31 11:41:09 +0300

    Add licence info and lispy doc headers

--- nu-notify.scm
+++ nu-notify.scm
@@ -1,5 +1,39 @@
-(use-modules (ice-9 format)
-             (redis))
+;;; (nu-notify) --- Notification module for Guile
+
+;; Copyright (C) 2019 Bonface M. K.
+
+;; Author: Bonface M. K.
+;; Keywords: notifacation, tools
+;; Package-Requires: ((redis) (ice-9 format))
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;; Commentary:
+
+;; Queue messages into redis
+
+;;; System Requirements:
+
+;;; Code:
+(define-module (nu-notify)
+  #:use-module (ice-9 format)
+  #:use-module (redis))
 
 (define (enqueue-error error-message conn)
   (let ((error-sexp (format #f "~a" error-message)))
@@ -15,3 +49,5 @@
         (enqueue-error `((command . ",cmd")
                          (pid . ,pid))
                  (redis-connect)))))
+
+;;; nu-notify ends here
