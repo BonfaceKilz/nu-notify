a6b097acc26d04f55c86032be2ce85a28296e00d
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-01-31 11:45:25 +0300

    Add licence info and lispy doc headers

--- worker.scm
+++ worker.scm
@@ -1,3 +1,35 @@
+;;; (nu-notify-worker) --- Notification module for Guile
+
+;; Copyright (C) 2019 Bonface M. K.
+
+;; Author: Bonface M. K.
+;; Keywords: notification, monitoring
+;; Package-Requires: ((redis))
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;; Commentary:
+
+;; Dequeue messages from redis. You should wrap this in a daemon
+
+;;; Code:
+
 (use-modules (redis))
 
 (define (dequeue-error conn)
@@ -5,3 +37,5 @@
     (display (redis-send conn (lpop '(queue:error))))
     (newline)
     (redis-close conn)))
+
+;;; nu-notify-worker ends here
