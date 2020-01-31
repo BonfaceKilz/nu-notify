84790db86ac20ecc7b00a352479f55c66471d07f
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-01-31 11:33:17 +0300

    Store error as an s-expression

--- nu-notify.scm
+++ nu-notify.scm
@@ -2,14 +2,16 @@
              (redis))
 
 (define (enqueue-error error-message conn)
-  (begin
-    (redis-send conn (lpush `(queue:error ,error-message)))
-    (redis-close conn)))
+  (let ((error-sexp (format #f "~a" error-message)))
+    (begin
+      (redis-send conn (lpush `(queue:error ,error-sexp)))
+      (redis-close conn))))
 
 (define (run-unix-cmd cmd)
   ;;; Run command command and on error,
   ;;; enqueue the PID to a redis queue
   (let ((pid (system cmd)))
     (if (> (status:exit-val pid) 0)
-        (enqueue-error (format #f "[Error] PID: ~d Command: ~a" pid cmd)
+        (enqueue-error `((command . ",cmd")
+                         (pid . ,pid))
                  (redis-connect)))))
